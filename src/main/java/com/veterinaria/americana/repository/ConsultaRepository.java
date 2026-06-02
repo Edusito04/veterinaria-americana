18f506360976a0959200674164d154dc593402ee
Edusito04 <eduthomasqm@gmail.com>
2026-06-02 18:24:01 -0500

    Update ConsultaRepository.java

--- src/main/java/com/veterinaria/americana/repository/ConsultaRepository.java
+++ src/main/java/com/veterinaria/americana/repository/ConsultaRepository.java
@@ -12,12 +12,12 @@ import java.util.List;
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     
-    // Consulta 1: Historial médico de una mascota ordenado por fecha
+   
     List<Consulta> findByMascotaIdOrderByFechaHoraDesc(Long mascotaId);
     
-    // Consulta 2: Agenda/Calendario del veterinario (Citas en un rango de tiempo, ej: hoy)
+  
     List<Consulta> findByFechaHoraBetween(LocalDateTime inicio, LocalDateTime fin);
     
-    // Reporte 1: Citas atendidas en un rango de fechas
+  
     List<Consulta> findByEstadoAndFechaHoraBetween(String estado, LocalDateTime inicio, LocalDateTime fin);
 }
