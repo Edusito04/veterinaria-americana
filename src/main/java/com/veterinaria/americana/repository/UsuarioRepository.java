4e2b4593b172fd25a0e3d4754d1814c51419e56e
Edusito04 <eduthomasqm@gmail.com>
2026-06-02 18:24:35 -0500

    Update UsuarioRepository.java

--- src/main/java/com/veterinaria/americana/repository/UsuarioRepository.java
+++ src/main/java/com/veterinaria/americana/repository/UsuarioRepository.java
@@ -7,9 +7,7 @@ import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Buscar por email para Spring Security (Autenticación)
     Optional<Usuario> findByEmail(String email);
     
-    // Validar si el correo ya existe al registrarse
     boolean existsByEmail(String email);
 }
